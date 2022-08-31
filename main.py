8e8deebd8cd715943d42b75007370cd1f905072c
luckyking666 <87979569+luckyking666@users.noreply.github.com>
2022-08-31 13:50:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def get_birthday(birthday, year, today):
  
  
 def get_ciba():
-    url = "https://api.vvhan.com/api/love"
+    url = "https://api.shadiao.pro/chp"
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
